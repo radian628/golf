4b33dbfaa0d83719bf93189eba4e8e966436f9f1
radian628 <break.a.radian@gmail.com>
2022-10-04 23:16:54 -0700

    added files

---
+++ json-parser/parser.mjs
@@ -0,0 +1,62 @@
+export let p = str => {
+    // get rid of leading and trailing whitespace
+    str = str.replace(/^\s+|\s+$/g,"");
+    
+    // function that splits an object/array by commas, taking into account objects/arrays inside of it
+    let splitByCommas = str => str
+        .slice(1,-1) // remove leading/trailing brackets 
+        .split("") // string to array
+        .reduce((prev, char) => { // convert the char array to an array of strings, split by commas
+            let shouldSplit = 
+                !prev[1] // do not split if the recursive depth is greater than 0. We don't want to split nested arrays/objects!
+                && !prev[3] // only split if not inside a string literal
+                && char == ","; // split at characters
+            return [
+                [
+                    prev[0].slice(0,-1), // previous substrings (split by commas)
+                    prev[0].slice(-1)+(shouldSplit?"":char),shouldSplit?[""]:[] // current substring, with potentially another character on it
+                   
+                ].flat(),
+                prev[1]+(prev[3]?0:((char=="{"||char=="[")||(-(char=="}"||char=="]")))), // increase recursive depth if encountering an open bracket; decrease if encountering a closed bracket char
+                char,    
+                (char=='"'&&prev[2]!="\\")?!prev[3]:prev[3]
+            ]},[
+                [], // prev[0]: array of substrings
+                0,  // prev[1]: recursion depth 
+                "", // prev[2]: previous character
+                0   // prev[3]: "is inside string?" (boolean)
+            ])[0];
+    
+    // lookup table for JSON types based on the first character in each type.
+    let jsonTypeLUTEntry = ({
+
+        // parse object
+        "{": _ => Object.fromEntries( // get object from array of key/value pairs
+            splitByCommas(str) // split object properties by commas
+            .map(e=>{ // for each object property...
+                let i = e.replace(/"[^"]*"/g, s=>s.replace(/./g,"a")).indexOf(":"); // find out where key/value split is
+                return [p(e.slice(0,i)),p(e.slice(i+1))] // return key/value pair
+            })),
+
+        // parse array
+        "[": _ => splitByCommas(str)
+            .map(p),
+        
+        // parse string 
+        '"': _ => str.slice(1,-1) // eliminate quotes
+            .replace(/\\(b|f|n|r|t|\\|\/)/g,r=>"\b\f\n\r\t\\\/"["bfnrt\\//".indexOf(r[1])]) //backslash escape sequences
+            .replace(/\\u.{4}/g,r=>String.fromCharCode(parseInt(r.slice(2),16))) // unicode escape sequences
+            
+        // null parser
+        ,"n":_=>null,
+        
+        // true parser
+        "t":_=>!0,
+        
+        // false parser
+        "f":()=>!1
+    })[str[0]]; // perform lookup
+    
+    // if lookup succeeded, evalute. If it failed, subtract zero to cast to number
+    return jsonTypeLUTEntry ? jsonTypeLUTEntry() : str-0;
+}
---
+++ json-parser/test.mjs
@@ -0,0 +1,75 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+
+import { p } from "./parser.mjs";
+
+function jsonEq(json1, json2) {
+    if (typeof json1 != typeof json2) return false;
+    if (json1 === null) {
+        return json2 === null;
+    }
+    switch (typeof json1) {
+    case "number":
+    case "string":
+    case "boolean":
+        return json1 == json2;
+    case "object":
+        if (Array.isArray(json1)) {
+            if (!Array.isArray(json2)) return false;
+            if (json1.length != json2.length) return false;
+            for (let i = 0; i < json1.length; i++) {
+                if (!jsonEq(json1[i], json2[i])) return false;
+            }
+            return true;
+        }
+        if (Array.isArray(json2)) return false;
+        const keys1 = new Set(Object.keys(json1));
+        const keys2 = new Set(Object.keys(json2));
+        for (let key of keys1.values()) {
+            if (!keys2.has(key)) return false;
+        }
+        for (let key of keys2.values()) {
+            if (!keys1.has(key)) return false;
+        }
+        for (let key of keys1.values()) {
+            if (!jsonEq(json1[key], json2[key])) return false;
+        }
+    }
+    return false;
+}
+
+console.log("Reading files...");
+
+const dir = (await fs.readdir("test_parsing")).filter(file => file[0] == "y");
+
+const fileContents = await Promise.all(dir.map(file => fs.readFile(path.join("test_parsing", file))));
+
+const fileInfo = fileContents.map((e, i) => [dir[i], e.toString()]);
+
+console.log("Parsing...");
+
+const parseResults = fileInfo.map(s => {
+    let golfed;
+    try {
+        golfed = p(s[1]);
+    } catch {
+        golfed = undefined
+    }
+    return {
+        reference: JSON.parse(s[1]),
+        golfed,
+        fileName: s[0],
+        fileContents: s[1]
+    }
+});
+
+console.log("Validating...");
+
+let output = "";
+for (let result of parseResults) {
+    output += result.fileName.padEnd(50, " ");
+    output += jsonEq(result.reference, result.golfed) ? "SUCCESS" : "FAIL";
+    output += "\n";
+}
+
+await fs.writeFile("results.txt", output);
