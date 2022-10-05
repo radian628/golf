611be784e8f2c19d03e57fc75ab54523652e9b5a
radian628 <break.a.radian@gmail.com>
2022-10-04 23:53:14 -0700

    passing all tests

--- json-parser/parser.mjs
+++ json-parser/parser.mjs
@@ -1,11 +1,15 @@
 export let p = str => {
+    let slice = "slice";
+    let map = "map"
+    let split = "split";
+    let replace = "replace";
     // get rid of leading and trailing whitespace
-    str = str.replace(/^\s+|\s+$/g,"");
+    str = str[replace](/^\s+|\s+$/g,"");
 
     // function that splits an object/array by commas, taking into account objects/arrays inside of it
     let splitByCommas = str => str
-        .slice(1,-1) // remove leading/trailing brackets 
-        .split("") // string to array
+        [slice](1,-1) // remove leading/trailing brackets 
+        [split]("") // string to array
         .reduce((prev, char) => { // convert the char array to an array of strings, split by commas
             let shouldSplit = 
                 !prev[1] // do not split if the recursive depth is greater than 0. We don't want to split nested arrays/objects!
@@ -13,8 +17,8 @@ export let p = str => {
                 && char == ","; // split at characters
             return [
                 [
-                    prev[0].slice(0,-1), // previous substrings (split by commas)
-                    prev[0].slice(-1)+(shouldSplit?"":char),shouldSplit?[""]:[] // current substring, with potentially another character on it
+                    prev[0][slice](0,-1), // previous substrings (split by commas)
+                    prev[0][slice](-1)+(shouldSplit?"":char),shouldSplit?[""]:[] // current substring, with potentially another character on it
                    
                 ].flat(),
                 prev[1]+(prev[3]?0:((char=="{"||char=="[")||(-(char=="}"||char=="]")))), // increase recursive depth if encountering an open bracket; decrease if encountering a closed bracket char
@@ -33,28 +37,26 @@ export let p = str => {
         // parse object
         "{": _ => Object.fromEntries( // get object from array of key/value pairs
             splitByCommas(str) // split object properties by commas
-            .map(e=>{ // for each object property...
-                let i = e.replace(/"[^"]*"/g, s=>s.replace(/./g,"a")).indexOf(":"); // find out where key/value split is
-                return [p(e.slice(0,i)),p(e.slice(i+1))] // return key/value pair
+            [map](e=>{ // for each object property...
+                let i = e[replace](/"[^"]*"/g, s=>s[replace](/./g,"a")).indexOf(":"); // find out where key/value split is
+                return [p(e[slice](0,i)),p(e[slice](i+1))] // return key/value pair
             })),
 
         // parse array
         "[": _ => splitByCommas(str)
-            .map(p),
+            [map](p),
         
         // parse string 
-        '"': _ => str.slice(1,-1) // eliminate quotes
-            // .replace(/\\(b|f|n|r|t|\\|\/|\")/g,r=>`\b\f\n\r\t\\\/"`[`bfnrt\\/"`.indexOf(r[1])]) //backslash escape sequences
-            // .replace(/\\u.{4}/g,r=>String.fromCharCode(parseInt(r.slice(2),16))) // unicode escape sequences
-            .split("\\\\")
-            .map(s => s
-                .split("\\")    
-                .map((s,i) => i==0?s:s
-                    .replace(/^(b|f|n|r|t|\/|\")/g,r=>`\b\f\n\r\t\/"`[`bfnrt/"`.indexOf(r)])
-                    .replace(/^u.{4}/g,r=>String.fromCharCode(parseInt(r.slice(1),16)))
+        '"': _ => str[slice](1,-1) // eliminate quotes
+            [split]("\\\\") // split by escaped backslashes
+            [map](s => s 
+                [split]("\\")    // split by escape code
+                [map]((s,i) => i==0?s:s // ignore first segment (does not start with escape code)
+                    [replace](/^(b|f|n|r|t|\/|\")/g,r=>`\b\f\n\r\t\/"`[`bfnrt/"`.indexOf(r)]) // deal with escape codes such as \n
+                    [replace](/^u.{4}/g,r=>String.fromCharCode(parseInt(r[slice](1),16))) // deal with unicode escape codes
                 )
-                .join("")    
-            ).join("\\")
+                .join("")   // join back together
+            ).join("\\") // re-add escaped backslashes
             
         // null parser
         ,"n":_=>null,
