f54559f9124dd459c86f79339c3d9bc69769e354
radian628 <break.a.radian@gmail.com>
2022-10-05 00:13:46 -0700

    further opts + minifier list

---
+++ json-parser/README.md
@@ -0,0 +1,4 @@
+# Best Minifiers so far
+
+2. https://www.digitalocean.com/community/tools/minify
+1. https://skalman.github.io/UglifyJS-online/
--- json-parser/parser.mjs
+++ json-parser/parser.mjs
@@ -1,15 +1,13 @@
 export let p = str => {
-    let slice = "slice";
-    let map = "map"
-    let split = "split";
-    let replace = "replace";
+    // a minifier will shorten these to a single character, allowing them to more compactly represent builtin functions
+    let [slice,map,split,replace,indexOf,empty,negativeOne] = ["slice","map","split","replace","indexOf","",-1];
     // get rid of leading and trailing whitespace
-    str = str[replace](/^\s+|\s+$/g,"");
+    str = str[replace](/^\s+|\s+$/g,empty);
 
     // function that splits an object/array by commas, taking into account objects/arrays inside of it
     let splitByCommas = str => str
-        [slice](1,-1) // remove leading/trailing brackets 
-        [split]("") // string to array
+        [slice](1,negativeOne) // remove leading/trailing brackets 
+        [split](empty) // string to array
         .reduce((prev, char) => { // convert the char array to an array of strings, split by commas
             let shouldSplit = 
                 !prev[1] // do not split if the recursive depth is greater than 0. We don't want to split nested arrays/objects!
@@ -17,17 +15,17 @@ export let p = str => {
                 && char == ","; // split at characters
             return [
                 [
-                    prev[0][slice](0,-1), // previous substrings (split by commas)
-                    prev[0][slice](-1)+(shouldSplit?"":char),shouldSplit?[""]:[] // current substring, with potentially another character on it
-                   
+                    prev[0][slice](0,negativeOne), // previous substrings (split by commas)
+                    prev[0][slice](negativeOne)+(shouldSplit?empty:char), // current substring, with potentially another character on it
+                    shouldSplit?empty:[] // potentially the next substring
                 ].flat(),
-                prev[1]+(prev[3]?0:((char=="{"||char=="[")||(-(char=="}"||char=="]")))), // increase recursive depth if encountering an open bracket; decrease if encountering a closed bracket char
+                prev[1]+(prev[3]?0:(("{["[indexOf](char)>negativeOne)||-("]}"[indexOf](char)>negativeOne))), // increase recursive depth if encountering an open bracket; decrease if encountering a closed bracket char
                 char,    
                 (char=='"'&&prev[2]!="\\")?!prev[3]:prev[3]
             ]},[
                 [], // prev[0]: array of substrings
                 0,  // prev[1]: recursion depth 
-                "", // prev[2]: previous character
+                empty, // prev[2]: previous character
                 0   // prev[3]: "is inside string?" (boolean)
             ])[0];
     
@@ -38,7 +36,7 @@ export let p = str => {
         "{": _ => Object.fromEntries( // get object from array of key/value pairs
             splitByCommas(str) // split object properties by commas
             [map](e=>{ // for each object property...
-                let i = e[replace](/"[^"]*"/g, s=>s[replace](/./g,"a")).indexOf(":"); // find out where key/value split is
+                let i = e[replace](/"[^"]*"/g, s=>s[replace](/./g,"a"))[indexOf](":"); // find out where key/value split is
                 return [p(e[slice](0,i)),p(e[slice](i+1))] // return key/value pair
             })),
 
@@ -47,15 +45,15 @@ export let p = str => {
             [map](p),
         
         // parse string 
-        '"': _ => str[slice](1,-1) // eliminate quotes
+        '"': _ => str[slice](1,negativeOne) // eliminate quotes
             [split]("\\\\") // split by escaped backslashes
             [map](s => s 
                 [split]("\\")    // split by escape code
                 [map]((s,i) => i==0?s:s // ignore first segment (does not start with escape code)
-                    [replace](/^(b|f|n|r|t|\/|\")/g,r=>`\b\f\n\r\t\/"`[`bfnrt/"`.indexOf(r)]) // deal with escape codes such as \n
+                    [replace](/^(b|f|n|r|t|\/|\")/g,r=>`\b\f\n\r\t\/"`[`bfnrt/"`[indexOf](r)]) // deal with escape codes such as \n
                     [replace](/^u.{4}/g,r=>String.fromCharCode(parseInt(r[slice](1),16))) // deal with unicode escape codes
                 )
-                .join("")   // join back together
+                .join(empty)   // join back together
             ).join("\\") // re-add escaped backslashes
             
         // null parser
